c12d23fab8e6b608d7aba5e8d0b5f9bbbaaaf8ec
vivekrawat21 <vivekrwt2111@gmail.com>
2024-05-15 01:46:06 +0530

    try to fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,43 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript:{
-        ignoreBuildErrors:true
+
+
+images: {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'img.clerk.com',
     },
-    experimental:{
-        serverActions: true,
-        serverComponentsExternalPackages:['mongoose'],
+    {
+      protocol: 'https',
+      hostname: 'images.clerk.dev',
     },
-    images: {
-        domains: [
-            'img.clerk.com',
-            'images.clerk.dev',
-            'uploadthing.com',
-            'placehold.co',
-            'utfs.io' // Add this line
-        ]
-,
-        remotePatterns:[
-            {
-                protocol: 'https',
-                hostname: 'img.clerk.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'images.clerk.dev',
-
-            },
-            {
-                protocol: 'https',
-                hostname: 'uploadthing.com',
-
-            },
-            {
-                protocol: 'https',
-                hostname: 'placehold.co',
-
-            }
-        ]
+    {
+      protocol: 'https',
+      hostname: 'uploadthing.com',
     },
-};
+    {
+      protocol: 'https',
+      hostname: 'placehold.co',
+    },
+    {
+      protocol: 'https',
+      hostname: 'utfs.io',
+    }
+  ]
+},
 
+    };
 export default nextConfig;
